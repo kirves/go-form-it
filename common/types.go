bcbd78156fdeae04a0dba38b3d1885781028c15c
Alessandro Frossi <frossialex@gmail.com>
2014-03-26 18:16:33 +0100

    Moved constants to common

---
+++ common/types.go
@@ -0,0 +1,34 @@
+package formcommon
+
+const (
+	BUTTON         = "button" //
+	CHECKBOX       = "checkbox"
+	COLOR          = "color"
+	DATE           = "date"
+	DATETIME       = "datetime"
+	DATETIME_LOCAL = "datetime-local"
+	EMAIL          = "email"
+	FILE           = "file"
+	HIDDEN         = "hidden"
+	IMAGE          = "image"
+	MONTH          = "month"
+	NUMBER         = "number"
+	PASSWORD       = "password" //
+	RADIO          = "radio"
+	RANGE          = "range"
+	RESET          = "reset" //
+	SEARCH         = "search"
+	SUBMIT         = "submit" //
+	TEL            = "tel"
+	TEXT           = "text" //
+	TIME           = "time"
+	URL            = "url"
+	WEEK           = "week"
+	TEXTAREA       = "textarea" //
+	SELECT         = "select"   //
+)
+
+const (
+	BASE      = "base"
+	BOOTSTRAP = "bootstrap3"
+)
