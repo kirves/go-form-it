9aaec6d48e8e61586671a912924dad2bce2f9136
Alessandro Frossi <frossialex@gmail.com>
2014-04-02 12:11:02 +0200

    Added generic button

--- fields/button.go
+++ fields/button.go
@@ -4,6 +4,11 @@ import (
 	"github.com/kirves/go-form-it/common"
 )
 
+// Generic button type
+type ButtonType struct {
+	Field
+}
+
 // Submit button type
 type SubmitButtonType struct {
 	Field
@@ -27,6 +32,15 @@ func SubmitButton(name string, text string) *SubmitButtonType {
 func ResetButton(name string, text string) *ResetButtonType {
 	ret := &ResetButtonType{
 		FieldWithType(name, formcommon.RESET),
+	}
+	ret.SetText(text)
+	return ret
+}
+
+// Button creates a default generic button
+func Button(name string, text string) *ButtonType {
+	ret := &ButtonType{
+		FieldWithType(name, formcommon.BUTTON),
 	}
 	ret.SetText(text)
 	return ret
